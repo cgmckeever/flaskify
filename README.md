50b11e3e6ada992128fa045c8a232f01ea28a8b8
cgmckeever <cgmckeever@giveforward.com>
2017-03-23 18:28:20 -0600

    sql alchemy

--- README.md
+++ README.md
@@ -27,6 +27,7 @@ The Framework
 
 `pip install flask`
 `pip install flask-wtf`
+`pip install flask-sqlalchemy`
 
 ## autoenv
 
--- app/views.py
+++ app/views.py
@@ -6,26 +6,31 @@ from .forms import LoginForm
 @app.route('/index')
 
 def index():
-    user = {'nickname': 'Miguel'}  # fake user
-    posts = [  # fake array of posts
-        { 
-            'author': {'nickname': 'John'}, 
-            'body': 'Beautiful day in Portland!' 
-        },
-        { 
-            'author': {'nickname': 'Susan'}, 
-            'body': 'The Avengers movie was so cool!' 
-        }
-    ]
-    return render_template("index.html",
-                           title='Home',
-                           user=user,
-                           posts=posts)
+  user = {'nickname': 'Miguel'}  # fake user
+  posts = [  # fake array of posts
+      {
+          'author': {'nickname': 'John'},
+          'body': 'Beautiful day in Portland!'
+      },
+      {
+          'author': {'nickname': 'Susan'},
+          'body': 'The Avengers movie was so cool!'
+      }
+  ]
+  return render_template("index.html",
+                         title='Home',
+                         user=user,
+                         posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 
 def login():
-  return render_template('login.html', 
+  form = LoginForm()
+  if form.validate_on_submit():
+    flash('Login requested for OpenID="%s", remember_me=%s' %
+          (form.openid.data, str(form.remember_me.data)))
+    return redirect('/index')
+  return render_template('login.html',
                           title='Sign In',
-                          form=LoginForm())
+                          form=form)
