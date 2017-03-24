d45bd95b797ddd934553537a599c2b9b42620bc3
cgmckeever <cgmckeever@giveforward.com>
2017-03-24 17:40:33 -0500

    indents

--- app/views.py
+++ app/views.py
@@ -9,14 +9,14 @@ from .forms import LoginForm
 def index():
   user = {'nickname': 'Miguel'}  # fake user
   posts = [  # fake array of posts
-      {
-          'author': {'nickname': 'John'},
-          'body': 'Beautiful day in Portland!'
-      },
-      {
-          'author': {'nickname': 'Susan'},
-          'body': 'The Avengers movie was so cool!'
-      }
+    {
+        'author': {'nickname': 'John'},
+        'body': 'Beautiful day in Portland!'
+    },
+    {
+        'author': {'nickname': 'Susan'},
+        'body': 'The Avengers movie was so cool!'
+    }
   ]
   return render_template("index.html",
                          title='Home',
