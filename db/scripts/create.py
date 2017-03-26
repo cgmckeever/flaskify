77d40f90d216de65a3c8cb5d835c4f287f995afa
cgmckeever <cgmckeever@giveforward.com>
2017-03-26 16:07:18 -0500

    database creation script

--- db/scripts/create.py
+++ db/scripts/create.py
@@ -1,5 +1,9 @@
 #!env/bin/python
 
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+
 from migrate.versioning import api
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
